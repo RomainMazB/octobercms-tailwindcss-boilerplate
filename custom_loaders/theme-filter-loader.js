4c86b9b9bf63ca686de1f9a3e321e2f47057bdf4
RomainMazB <contact@maz.digital>
2020-04-26 15:59:41 +0200

    Fix & Code Simplification

--- custom_loaders/theme-filter-loader.js
+++ custom_loaders/theme-filter-loader.js
@@ -4,20 +4,18 @@ let path = require('path'),
     themeRegex = /\{\{[\s?]*["|'|`](.*[\.[png|jpe?g|gif|ico])["|'|`][\s?]*\|[\s?]*theme[\s?]*\}\}/g;
 
 module.exports = function (content, map, meta) {
-    let filterTags = content.match(themeRegex)
-    const options = loaderUtils.getOptions(this)
-    if (filterTags) {
-        output = true
-        filterTags = [...new Set(filterTags)]
-        filterTags.map(filterTag => {
-            let parts = themeRegex.exec(filterTag),
-                fullPath = path.join(options.from, parts[1]),
+    let options = loaderUtils.getOptions(this)
+    if (content.match(themeRegex)) {
+        let parts;
+        while ((parts = themeRegex.exec(content)) !== null) {
+            let fullPath = path.join(options.from, parts[1]),
                 file = fs.readFileSync(fullPath)
             this.emitFile(parts[1], file)
-        })
+        }
     }
 
     return 'module.exports = [\n' +
         JSON.stringify(content) +
         '\n].join();'
 }
+
