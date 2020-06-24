5a2ae8029ff47d7d274d9c48833c0cdcde6852b3
RomainMazB <romainmazb@gmail.com>
2020-06-24 22:15:47 +0200

    Move PurgeCSS to TW Built-in process

--- blank-src/purgecss.config.js
+++
@@ -1,8 +0,0 @@
-let glob = require('glob'),
-    path = require('path')
-
-module.exports = {
-    paths: glob.sync(path.join(__dirname, '**/*.htm'),  { nodir: true }),
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    whitelist: []
-}
--- blank-src/tailwind.config.js
+++ blank-src/tailwind.config.js
@@ -1,6 +1,12 @@
+let glob = require('glob'),
+    path = require('path')
+
 module.exports = {
     theme: {
         extend: {
         }
-    }
+    },
+    variants: {},
+    plugins: [],
+    purge: glob.sync(path.join(__dirname, '**/*.htm')),
   }
