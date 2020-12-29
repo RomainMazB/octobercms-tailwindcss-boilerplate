5479ccc145d910a7bcc18a104fc67eac17d2e0c7
RomainMazB <romainmazb@gmail.com>
2020-12-29 16:40:34 +0100

    Add Tailwind 2.0 support

--- blank-src/tailwind.config.js
+++ blank-src/tailwind.config.js
@@ -2,11 +2,14 @@ let glob = require('glob'),
     path = require('path')
 
 module.exports = {
+    purge: glob.sync(path.join(__dirname, '**/*.htm')),
     theme: {
-        extend: {
-        }
+        extend: {},
     },
     variants: {},
-    plugins: [],
-    purge: glob.sync(path.join(__dirname, '**/*.htm')),
-  }
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+    ],
+}
